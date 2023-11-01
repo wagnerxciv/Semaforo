2aaed375c291f203c21656e0c610f5035b1a3e56
wagnerxciv <125404234+wagnerxciv@users.noreply.github.com>
2023-11-01 16:55:28 -0300

    Add files via upload

---
+++ bodacious.ino
@@ -0,0 +1,24 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(13, HIGH);
+  delay(10000); 
+  digitalWrite(13, LOW);
+  delay(1000); 
+  digitalWrite(12, HIGH);
+  delay(2000); 
+  digitalWrite(12, LOW);
+  delay(1000); 
+  digitalWrite(11, HIGH);
+  delay(8000); 
+  digitalWrite(11, LOW);
+  delay(1000); 
+}
